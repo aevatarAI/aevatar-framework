5bd8cb0ce7a295592ceceadfb340e5448f7a0793
zhxymh <sean.zhang@aelf.io>
2025-05-20 11:26:05 +0800

    feat: rename IPermissionState

--- src/Aevatar.PermissionManagement/PermissionStateBase.cs
+++ src/Aevatar.PermissionManagement/PermissionStateBase.cs
@@ -4,14 +4,14 @@ using Aevatar.Core.Abstractions;
 
 namespace Aevatar.PermissionManagement;
 
-public interface IPermissionStateBase
+public interface IPermissionState
 {
     bool IsPublic { get; set; }
-   HashSet<Guid> AuthorizedUserIds { get; set; }
+    HashSet<Guid> AuthorizedUserIds { get; set; }
 }
 
 [GenerateSerializer]
-public abstract class PermissionStateBase : StateBase, IPermissionStateBase
+public abstract class PermissionStateBase : StateBase, IPermissionState
 {
     [Id(0)] public bool IsPublic { get; set; } = true;
     [Id(1)] public HashSet<Guid> AuthorizedUserIds { get; set; } = new();
