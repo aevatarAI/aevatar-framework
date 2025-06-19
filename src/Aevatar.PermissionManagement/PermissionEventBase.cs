96f5cccbc122d32cd4ab7ae6e53f0ef3927a6544
t0wnsend <townsend@ebridge.exchange>
2025-06-19 14:19:51 +0800

    refactor: simplify UserContext initialization using property initializer

--- src/Aevatar.PermissionManagement/PermissionEventBase.cs
+++ src/Aevatar.PermissionManagement/PermissionEventBase.cs
@@ -10,10 +10,5 @@ public interface IPermissionEvent
 [GenerateSerializer]
 public abstract class PermissionEventBase : EventBase, IPermissionEvent
 {
-    protected PermissionEventBase()
-    {
-        UserContext = new UserContext();
-    }
-
-    [Id(0)] public UserContext? UserContext { get; set; }
+    [Id(0)] public UserContext? UserContext { get; set; } = new();
 }
